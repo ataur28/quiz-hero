d5d19d66cc51e6373221e7c67156e37ce878be32
ataur28 <ataurrahman862828@gmail.com>
2023-03-16 02:58:41 +0600

    fixed bug in qizeContainer with createElement and appendChild

--- index.js
+++ index.js
@@ -60,17 +60,22 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
-    quizContainer.innerHTML = `<div class="m-3 py-3 px-4 shadow-sm rounded">
+    console.log(i + 1);
+    console.log(quiz.question);
+    console.log(quiz.options);
+    const quizContainerQuestion = document.createElement('div');
+    quizContainerQuestion.innerHTML = `<div class="m-3 py-3 px-4 shadow-sm rounded">
                                   <div class="flex items-center">
                                     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
                                     ${i + 1}
                                     </div>
-                                    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+                                    <p class="text-gray-800 text-sm">${quiz.question}</p>
                                   </div>
                                   <div class="grid grid-cols-2 gap-4 mt-5">
                                      ${displayQuizOptions(quiz.options, i)}
                                   </div>
                                 </div>`;
+                                quizContainer.appendChild(quizContainerQuestion);
   });
 };
 
